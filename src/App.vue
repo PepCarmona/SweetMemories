abc15e4c8389cf687a9f93a7e5e3c23f7c74eb21
Pep Carmona <pep.carmona.coll@gmail.com>
2024-07-24 18:13:12 +0200

    render page conditionally

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import TheHeader from './components/TheHeader.vue';
+import PageLoader from './components/PageLoader.vue';
+import { useAuth0 } from '@auth0/auth0-vue';
+
+const { isLoading } = useAuth0();
 </script>
 
 <template>
-  <TheHeader />
+  <PageLoader v-if="isLoading" />
 
-  <RouterView />
+  <template v-else>
+    <TheHeader />
+
+    <RouterView />
+  </template>
 </template>
---
+++ src/components/PageLoader.vue
@@ -0,0 +1,1 @@
+<template>Loading Page</template>
