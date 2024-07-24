73218ee5f3b0cc9885174128bb825c3757b8cbba
Pep Carmona <pep.carmona.coll@gmail.com>
2024-07-24 18:16:40 +0200

    add auth guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../pages/HomePage.vue';
+import { authGuard } from '@auth0/auth0-vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,12 @@ const router = createRouter({
       path: '/callback',
       name: 'callback',
       component: () => import('../pages/CallbackPage.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../pages/ProfilePage.vue'),
+      beforeEnter: authGuard,
     },
   ],
 });
