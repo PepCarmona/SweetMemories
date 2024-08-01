bd69e00d983d0907e27fb0a57cb7d82c2c8bc972
Pep Carmona <pep.carmona.coll@gmail.com>
2024-08-01 17:00:11 +0200

    create base styles

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,13 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/assets/mixins.scss";',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
