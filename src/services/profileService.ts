c819e5467a0a8085344a0162e45654bd74b39278
Pep Carmona <pep.carmona.coll@gmail.com>
2024-09-01 18:19:33 +0200

    create profile supabaase

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,5 +1,18 @@
+import type { UserProfile } from '@/types/user';
 import { useSupabaseClient } from './supabase';
 
 export function useProfileService() {
   const client = useSupabaseClient();
+
+  async function createProfile(profile: UserProfile): Promise<void> {
+    const { error } = await client.from('profile').insert(profile);
+
+    if (error !== null) {
+      return Promise.reject(error);
+    }
+  }
+
+  return {
+    createProfile,
+  };
 }
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import type { Database } from '@/types/database';
 
 export function useSupabaseClient() {
-  const client = createClient(
+  const client = createClient<Database>(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_KEY
   );
